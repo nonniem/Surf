e2b74d5687757c6ae26ac4d0874f9134e56d9185
nonniem <martin.nonnie@gmail.com>
2018-11-19 16:03:18 -0600

    css

--- src/main/Body/Gallery.js
+++ src/main/Body/Gallery.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import Siani from "./media/siani.jpg";
+import tbag from './media/tbag.jpg';
+import witt from './media/witt.jpg';
+import chris from './media/siani.jpg';
 
 function Gallery(){
   return(
     <div>
       <h2>lotza pictures in hur</h2>
-      <img src='Siani' width='150px' height='200px'></img>
+      <img src={tbag} alt='' width='250px' height='200px' style={{padding:'20px'}}></img>
+      <img src={witt} alt='' width='250px' height='200px' style={{padding:'20px'}}></img>
+      <img src={chris} alt='' width='250px' height='200px' style={{padding:'20px'}}></img>
     </div>
   )}
 
